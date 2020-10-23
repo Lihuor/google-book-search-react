291401f3d54bed72d8a87fecd20bef645c1fa3b0
Lihuor <lihuor@gmail.com>
2020-10-23 14:25:24 +1100

    fixed style

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-import style from "./style.css";
+import "./style.css";
 
 function Header() {
     return (
         <div className="jumbotron text-center">
             <div className="container text-light">
-                <h1>Google Books Search</h1>
+                <h1>GOOGLE Books Search React</h1>
             </div>
         </div>
     );
